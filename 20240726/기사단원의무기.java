cb2fa2789c79c8596c3f5db402b0b0da2fc7ad83
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 10:06:55 +0900

    20240726 소스코드 업로드

---
+++ 20240726/기사단원의무기.java
@@ -0,0 +1,21 @@
+class 기사단원의무기 {
+    public int solution(int number, int limit, int power) {
+        int iron =0;
+        int total=0;
+        for(int i=1;i<=number;i++){
+            int cnt = getMea(i);
+            int weap = (cnt>limit)?power:cnt;
+            total+=weap;
+        }
+        return total;
+
+    }
+    static int getMea(int num){
+        int cnt=0;
+        for(int i=1;i<Math.sqrt(num);i++)
+            if(num%i==0)cnt++;
+        cnt*=2;
+        if(num%Math.sqrt(num)==0)cnt++;
+        return cnt;
+    }
+}
