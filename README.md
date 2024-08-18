41479fa4215ede641e476a3c468bfa52eb75dff2
Changmin Kim <ksjchm4@gmail.com>
2024-08-18 13:07:57 +0900

    20240818 자바 1

--- README.md
+++ README.md
@@ -196,3 +196,7 @@
 #### 자바
 1. [귤고르기](https://school.programmers.co.kr/learn/courses/30/lessons/138476)
 
+---
+**20240818**
+#### 자바
+1. [괄호회전](https://school.programmers.co.kr/learn/courses/30/lessons/76502)
---
+++ code202408/20240818/괄호회전.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class 괄호회전 {
+    public int solution(String s) {
+        int ret = 0;
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            String rotated = s.substring(i) + s.substring(0, i);  // 문자열 회전
+            // 위 코드는 s.substring(i)로 idx=i부터 끝까지 + s.substring(0, i)로 0~i-1 까지
+            if (isValid(rotated)) {
+                ret++;
+            }
+        }
+
+        return ret;
+    }
+
+    private boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c); //여는 괄호는 무조건 push
+            }
+            else {
+                if (stack.isEmpty()) { //처음에 닫는 괄호면 무조건 실패
+                    return false;
+                }
+                char top = stack.pop(); //아니면 비교
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
