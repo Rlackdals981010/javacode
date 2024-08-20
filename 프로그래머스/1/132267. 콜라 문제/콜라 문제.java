2401273c1b35cf6a1e50e55176c4e490aa06796d
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:47:45 +0900

    [level 1] Title: 콜라 문제, Time: 0.15 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int ret =0;
+        int now = n;
+        int rest=0;
+        while(now>=a){            
+            int get = now/a*b;
+            rest = now%a;
+            ret+=get;
+            now=get+rest;
+        }
+        return ret;
+        
+    }
+}
