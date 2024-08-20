c6e961eaf87ac244b6c1024d85f4692b2aade0d4
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:46:02 +0900

    [level 1] Title: 숫자 짝꿍, Time: 80.77 ms, Memory: 154 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String X, String Y) {
+        int[] x = new int[10];
+        int[] y = new int[10];        
+                
+        for(int i=0;i<X.length();i++){
+            int idx=X.charAt(i)-'0';
+            x[idx]++;
+        }
+        
+        for(int i=0;i<Y.length();i++){
+            int idx=Y.charAt(i)-'0';
+            y[idx]++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=9;i>=0;i--){
+            int cnt = Math.min(x[i],y[i]);
+            for(int j=0;j<cnt;j++)
+                sb.append(i);            
+        }
+        String ret = sb.toString();
+        
+        if(ret.equals(""))return "-1";
+        if (ret.charAt(0) == '0') {
+            return "0";
+        }
+        return ret;
+       
+             
+    }
+}
