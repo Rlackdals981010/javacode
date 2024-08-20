60a74801581087aa399a18293ac405e9587053ce
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:48:16 +0900

    [level 1] Title: K번째수, Time: 0.57 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] ret = new int[commands.length];
+        for(int i=0;i<ret.length;i++){
+            int start= commands[i][0]-1;
+            int last= commands[i][1]-1;
+            int target= commands[i][2]-1;
+            if(start==last){
+                ret[i]=array[start];
+            }
+            else{
+                int len = last-start+1;
+                int[] arr = new int[len];
+                for(int j=start;j<=last;j++){
+                    arr[j-start]=array[j];
+                }
+                Arrays.sort(arr);
+            
+                ret[i]=arr[target];
+            }
+            
+        }
+        return ret;
+    }
+}
