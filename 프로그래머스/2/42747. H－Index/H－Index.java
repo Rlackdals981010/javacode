23e3574016f26e34db40c77e02de1ddbd6cf6967
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:37:30 +0900

    [level 2] Title: H-Index, Time: 0.87 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations); // 오름차순 정렬
+        int hIndex = 0;
+
+        for(int i = 0; i < citations.length; i++) {
+            int h = citations.length - i; 
+            if (citations[i] >= h) { // 인용 횟수가 h 이상일 때
+                hIndex = h;
+                break; // 1번 h가 최댓값임
+            }
+        }
+        
+        return hIndex;
+    }
+}
