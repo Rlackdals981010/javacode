0f001fa6040752c4233fda1c15888f59160bbe49
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:39:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1576.87 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        HashSet<Integer> uniqueSums = new HashSet<>();
+        int n = elements.length;
+
+        for (int counter = 1; counter <= n; counter++) {
+            for (int i = 0; i < n; i++) { //시작점
+                int sum = 0;
+                for (int j = 0; j < counter; j++) { //카운터
+                    sum += elements[(i + j) % n];
+                }
+                uniqueSums.add(sum);
+            }
+        }
+
+        return uniqueSums.size();
+    }
+}
