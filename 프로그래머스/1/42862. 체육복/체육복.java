1290c342a23e2471566e49cc593ee1c5411890d3
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:45:52 +0900

    [level 1] Title: 체육복, Time: 1.50 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int ret = n - lost.length;        
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
+        
+        //여분제거
+        for(int i=0;i<lost.length;i++){
+            for(int j=0;j<reserve.length;j++){
+                if(lost[i]==reserve[j]){
+                    lost[i]=-1;
+                    reserve[j]=-1;
+                    ret++;
+                    break;
+                }
+            }
+        }         
+        int idx=0;
+        
+        //빌려주기        
+        for(int i=0;i<lost.length;i++){
+            for(int j=idx;j<reserve.length;j++){
+                if(lost[i]-1==reserve[j]||lost[i]+1==reserve[j]){
+                    reserve[j]=-1;
+                    lost[i]=-1;
+                    ret++;
+                    idx=j+1;
+                    break;
+                }
+            }
+        }
+        return ret;        
+    }
+}
