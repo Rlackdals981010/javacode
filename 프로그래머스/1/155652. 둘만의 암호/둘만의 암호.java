d77c7a2918552ea9940a644bc0601bb4c2a9d4df
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:45:25 +0900

    [level 1] Title: 둘만의 암호, Time: 0.77 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String s, String skip, int index) {               
+        StringBuilder sb = new StringBuilder();
+        
+        Set<Character> hs = new HashSet<>(); // 혹시라도 있을 중복 skip 문자 처리        
+        for(char ch : skip.toCharArray()){
+            hs.add(ch);
+        }
+        
+        for(char val : s.toCharArray()){
+            int cnt=0; //증가 카운터
+            char ret = val;
+            while(cnt<index){
+                ret++;
+                if(ret>'z')ret='a';
+                if(!hs.contains(ret))cnt++;
+            }
+            sb.append(ret);
+        }
+        return sb.toString();        
+    }
+}
