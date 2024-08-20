6066ba09062c0a97a818f2583119828893b9feaf
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:49:11 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.46 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {        
+        String[] str = s.split(" ",-1);
+        String ret="";
+        
+        for(int i=0;i<str.length;i++){
+            StringBuilder sb = new StringBuilder(str[i]);
+            for(int idx=0;idx<sb.length();idx++){
+                if(idx%2==0)sb.setCharAt(idx,Character.toUpperCase(sb.charAt(idx)));
+                else sb.setCharAt(idx,Character.toLowerCase(sb.charAt(idx)));
+            }
+            str[i]=sb.toString();
+        }
+        return String.join(" ",str);
+    }
+}
