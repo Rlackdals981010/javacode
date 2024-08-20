fdc1b15854ce74048e12a0f45e4e5e70269d24cd
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:44:16 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.69 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        String[] use_today = today.split("\\.");
+        long total = chang_day(use_today);
+        
+        String[][] use_terms = new String[terms.length][2];
+        for (int i = 0; i < terms.length; i++) {
+            String[] sp_terms = terms[i].split(" ");
+            use_terms[i][0] = sp_terms[0];
+            use_terms[i][1] = sp_terms[1];
+        }
+        
+        for (int i = 0; i < privacies.length; i++) {
+            String[] sp_privacies = privacies[i].split(" ");
+            String[] use_privacies = sp_privacies[0].split("\\.");
+            long ret = chang_day(use_privacies);
+            for (int j = 0; j < use_terms.length; j++) {
+                if (sp_privacies[1].equals(use_terms[j][0])) {
+                    ret += Integer.parseInt(use_terms[j][1]) * 28;
+                    break;
+                }
+            }
+            if (total >= ret)
+                arr.add(i + 1);
+        }
+        
+        int[] sol = new int[arr.size()];
+        for(int i=0;i<arr.size();i++){
+            sol[i]=arr.get(i);
+        }
+        
+        return sol;
+    }
+    
+    public static long chang_day(String[] use) {
+        long total = 0;
+        total += Long.parseLong(use[2]);
+        total += (Long.parseLong(use[1]) - 1) * 28;
+        total += (Long.parseLong(use[0]) - 1) * 28 * 12;
+        
+        return total;
+    }
+}
