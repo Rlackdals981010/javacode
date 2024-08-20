e87a64a1c9ae279a0c76c1c0973023d4aab4b51e
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:48:40 +0900

    [level 1] Title: 시저 암호, Time: 1.08 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            char use = s.charAt(i);
+            if(use==' ')sb.append(' ');
+            else{
+                if(use>='a'){
+                    if(use+n>'z')           sb.append((char)(use+n-('z'-'a'+1)));
+                    else                    sb.append((char)(use+n));
+                }
+                else{
+                    if (use+n>'Z')          sb.append((char)(use+n-('Z'-'A'+1)));
+                    else                    sb.append((char)(use+n));
+                }          
+            }
+        }
+        return sb.toString();
+    }
+}
