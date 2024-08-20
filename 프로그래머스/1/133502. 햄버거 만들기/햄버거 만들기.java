515af2977b0aee1bb0537c86313c935ef99291c4
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:45:16 +0900

    [level 1] Title: 햄버거 만들기, Time: 40.93 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int ret = 0;
+
+        for (int val : ingredient) {
+            arr.add(val);
+            
+            if (arr.size() >= 4) {
+                if(arr.get(arr.size() - 1) == 1 &&
+                  arr.get(arr.size() - 2) == 3 &&
+                  arr.get(arr.size() - 3) == 2 &&
+                  arr.get(arr.size() - 4) == 1 ){
+                    ret++;
+                    arr.remove(arr.size() - 1);
+                    arr.remove(arr.size() - 1);
+                    arr.remove(arr.size() - 1);
+                    arr.remove(arr.size() - 1);
+                }
+                    
+            }
+        }
+        return ret;
+    }
+}
