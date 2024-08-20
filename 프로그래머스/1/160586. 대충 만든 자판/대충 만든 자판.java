04c08e1929940cd5fa823e209c80c961b29b8f76
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-20 10:45:34 +0900

    [level 1] Title: 대충 만든 자판, Time: 13.34 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] ret = new int[targets.length];
+        
+        for(int i = 0; i < targets.length; i++) { // 각 타겟에 대해
+            String target = targets[i];                        
+            
+            for(char point : target.toCharArray()) { // 타겟의 각 문자에 대해
+                int min =102;
+                
+                for(String key : keymap) { // 각 키맵 문자열에 대해
+                    int index = key.indexOf(point);
+                    if(index != -1) {
+                        min = Math.min(min, index + 1);
+                    }
+                }
+                
+                if(min == 102) { // 문자를 찾을 수 없는 경우
+                    ret[i] = -1;                    
+                    break;
+                } else {
+                    ret[i] += min;
+                }
+                
+            }
+        }
+        
+        return ret;
+    }
+}
