ca353ce353390b4f4f4c01f8723bd3d4558b811a
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-08-28 20:38:30 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 17.71 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, int k) {
+        int ret =0;
+        String transNum = Integer.toString(n, k);
+        String[] transNumArr = transNum.split("0+"); //1001 -> [1],[1]
+        for(String use :transNumArr){
+            if(!use.isEmpty()&&check(Long.parseLong(use))){
+                ret++;
+            }
+        }
+        return ret;
+    
+    }
+    private boolean check(Long num){
+        if(num<2)return false;
+        for(long i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0)return false;
+        }
+        return true;
+    }
+}
