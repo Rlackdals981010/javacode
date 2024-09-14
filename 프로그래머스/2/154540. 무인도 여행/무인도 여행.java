382a89c30216a1928d94b68f83323f9d968168d6
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2024-09-14 14:34:27 +0900

    Update 무인도 여행.java

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -22,23 +22,24 @@ class Solution {
         
         ArrayList<Integer> arr = new ArrayList<>();
         
-        // 섬의 시작 좌표 찾기
+        // 섬의 시작 좌표 
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[0].length; x++) {
                 if (!check[y][x] && map[y][x] != 'X') {
-                    // 섬을 발견했을 때 DFS 호출
-                    arr.add(dfs(y, x, Character.getNumericValue(map[y][x])));
+                    
+                    arr.add(dfs(y, x, map[y][x]-'0'));
                 }
             }
         }
         
-        // 결과 정렬
+        
         Collections.sort(arr);
 
-        // 정렬된 결과를 배열로 변환
+        
         if (arr.size() == 0) {
-            return new int[]{-1}; // 섬이 없는 경우 -1 반환
+            return new int[]{-1}; 
         }
+        
         int[] ret = new int[arr.size()];
         for (int i = 0; i < arr.size(); i++) {
             ret[i] = arr.get(i);
@@ -46,24 +47,24 @@ class Solution {
         return ret;
     }
     
-    // DFS 탐색 함수
+   
     private int dfs(int y, int x, int nowTotal) {
-        // 현재 위치 방문 표시
+        
         check[y][x] = true;
 
-        // 상하좌우로 탐색
+        
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
 
-            // 배열의 경계 체크
+            
             if (ny >= map.length || nx >= map[0].length || ny < 0 || nx < 0) {
                 continue;
             }
 
-            // 방문하지 않았고, 'X'가 아닌 곳이면 DFS로 탐색
+            
             if (!check[ny][nx] && map[ny][nx] != 'X') {
-                nowTotal += dfs(ny, nx, Character.getNumericValue(map[ny][nx]));
+                nowTotal += dfs(ny, nx, map[ny][nx]-'0');
             }
         }
         return nowTotal;
