6b5ff42395ed0100edce50c77d286962ceaa2646
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-03-19 21:38:23 +0900

    [level 1] Title: 최소직사각형, Time: 2.45 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,11 +1,27 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int left=-1;
-        int right=-1;
-        for(int[] val : sizes){
-            left = Math.max(left,Math.max(val[0],val[1]));
-            right = Math.max(right,Math.min(val[0],val[1]));
+        int big = findBig(sizes[0][0],sizes[0][1]);
+        int small = findSmall(sizes[0][0],sizes[0][1]);
+        
+        for(int i=1;i<sizes.length;i++){
+            int newBig = findBig(sizes[i][0],sizes[i][1]);
+            int newSmall = findSmall(sizes[i][0],sizes[i][1]);
+            
+            if(big<newBig){
+                big=newBig;
+            }
+            if(small<newSmall){
+                small=newSmall;
+            }            
+            
         }
-        return left*right;
+        return big*small;
+        
+    }
+    private int findBig(int a, int b){
+        return a>=b?a:b;
+    }
+    private int findSmall(int a, int b){
+        return a>=b?b:a;
     }
 }
