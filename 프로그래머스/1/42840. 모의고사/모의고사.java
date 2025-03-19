e854f68094d1e2fb99075b2e3fcb84d731533191
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-03-19 22:10:22 +0900

    [level 1] Title: 모의고사, Time: 0.91 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,33 +1,34 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] arr1= {1,2,3,4,5};
-        int[] arr2= {2,1,2,3,2,4,2,5};
-        int[] arr3= {3,3,1,1,2,2,4,4,5,5};
-        int c1=0;
-        int c2=0;
-        int c3=0;
+        int[] first = {1, 2, 3, 4, 5};
+        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        for(int i=0;i<answers.length;i++){
-            int coll = answers[i];
-            if(coll==arr1[i%arr1.length])c1++;
-            if(coll==arr2[i%arr2.length])c2++;
-            if(coll==arr3[i%arr3.length])c3++;
+        int[] scores = new int[3];
+        
+        for (int i = 0; i < answers.length; i++){
+            if(answers[i] == first[i%first.length]){scores[0]++;}
+            if(answers[i] == second[i%second.length]){scores[1]++;}
+            if(answers[i] == third[i%third.length]){scores[2]++;}
         }
         
-        List<Integer> arr = new ArrayList<>();
-        int max = Math.max(c1,Math.max(c2,c3));
-        if(c1==max)arr.add(1);
-        if(c2==max)arr.add(2);
-        if(c3==max)arr.add(3);
-        
-        int[] ret = new int[arr.size()];
-        for(int i=0;i<ret.length;i++){
-            ret[i]=arr.get(i);
+        int max = Math.max(scores[0],Math.max(scores[1],scores[2]));
+        List<Integer> ret = new ArrayList<>();
+        for(int i=0;i<3;i++){
+            if(max==scores[i]){
+                ret.add(i+1);
+            }
         }
-        return ret;
         
+        int[] result = new int[ret.size()];
+        for(int i=0;i<result.length;i++){
+            result[i] = ret.get(i);
+        }
+        return result;
         
-        
+
     }
+    
+
 }
