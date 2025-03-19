a45f7b1ea834bc38ef5620ae51610e8065937c06
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-03-20 00:13:20 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,44 +1,25 @@
 import java.util.*;
-
 class Solution {
-    
-    private HashMap<Integer,Deque<Integer>> map = new HashMap<>();
-    private boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
-        
-        int ret =0;
-        visited = new boolean[computers.length];
-        
-        for(int i = 0 ; i< computers.length;i++){
-            Deque<Integer> deque = new LinkedList<>();
-            for(int ii=0;ii<computers[i].length;ii++){
-                if(computers[i][ii]==1&&i!=ii){
-                    deque.offer(ii);
+        boolean[] vis = new boolean[n];    
+        int ret = 0;
+        for(int i=0;i<computers.length;i++){
+            if(!vis[i]){                
+                ret++;
+                connectNetwork(i,vis,computers,n);
+            }
+        }
+        return ret;
+    }
+    private void connectNetwork(int idx,boolean[] vis,int[][] computers, int n){
+        vis[idx]=true;
+        for(int i=0;i<n;i++){
+            if(!vis[i]&&i!=idx){
+                if(computers[idx][i]==1){
+                    connectNetwork(i,vis,computers,n);
                 }
             }
-            map.put(i,deque);
-        }    
-        
-        for(int i=0;i<computers.length;i++){
-            if(visited[i]==false){
-                checkMap(computers,i);
-                ret++;
-            }
-        }
-        
-        return ret;
-        
-    }
-    private void checkMap(int[][] computers,int start){
-        visited[start]=true;
-        
-        Deque<Integer> deque = map.get(start);
-        
-        for(int val : deque){            
-            if(visited[val]==false){
-                checkMap(computers,val);
-            }
         }
     }
+    
 }
