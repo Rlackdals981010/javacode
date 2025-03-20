b99068e752d0c804502dca8a89c594d3fde90069
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-03-20 10:28:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.19 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,49 +2,39 @@ import java.util.*;
 
 class Solution {
     
-    private static final int[] dy = {-1, 0, 1, 0};
-    private static final int[] dx = {0, 1, 0, -1};
+    static final int[] dy = {-1, 0, 1, 0}; 
+    static final int[] dx = {0, 1, 0, -1}; 
     
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;        
-    
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, 1));         
         
-        boolean[][] visited = new boolean[n][m];
-        visited[0][0] = true;
+        Queue<Integer> yQueue = new LinkedList<>();
+        Queue<Integer> xQueue = new LinkedList<>();
         
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            int x = node.x;
-            int y = node.y;
-            int distance = node.distance;            
+        int[][] retMap = new int[maps.length][maps[0].length];
+        retMap[0][0]=1;
+        
+        yQueue.offer(0);
+        xQueue.offer(0);
+        while(!yQueue.isEmpty()&&!xQueue.isEmpty()){
+            int y = yQueue.poll();
+            int x = xQueue.poll();
             
-            if (x == m - 1 && y == n - 1) {
-                return distance;
-            }            
-            
-            for (int i = 0; i < 4; i++) {
+            for(int i=0;i<4;i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                                
-                if (ny >= 0 && ny < n && nx >= 0 && nx < m && maps[ny][nx] == 1 && !visited[ny][nx]) {
-                    visited[ny][nx] = true; 
-                    queue.add(new Node(nx, ny, distance + 1));
+                
+                if(ny<0||ny>=maps.length||nx<0||nx>=maps[0].length||maps[ny][nx]==0||retMap[ny][nx]!=0){
+                    continue;
                 }
-            }
-        }        
-     
-        return -1;
-    }    
-    
-    static class Node {
-        int x, y, distance;
-        Node(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
+                if (retMap[ny][nx] == 0) { // 처음 방문하는 경우만 갱신
+                    retMap[ny][nx] = retMap[y][x] + 1;
+                    yQueue.offer(ny);
+                    xQueue.offer(nx);
+                }                
+            }           
+            
         }
+        return retMap[maps.length-1][maps[0].length-1]==0?-1:retMap[maps.length-1][maps[0].length-1];
+       
     }
 }
