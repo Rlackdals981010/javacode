b5c5a736b8af6c3ee3c858dfd62f40c37c30cbf0
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-03-21 10:47:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.38 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> names = new HashMap<>();
+        for(String name:participant){
+            names.put(name,names.getOrDefault(name,0)+1);
+        }
+        for(String name : completion){
+            names.put(name,names.get(name)-1);
+        }
+        for(String name : names.keySet()){
+            if(names.get(name)!=0){
+                return name;
+            }
+        }
+        return "";
+    
+    }
+}
