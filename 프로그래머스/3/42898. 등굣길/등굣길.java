4d383cc8858b96d7ba8e17bd564e2ae14392dedb
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-03-21 20:45:50 +0900

    [level 3] Title: 등굣길, Time: 2.23 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,46 +1,29 @@
 class Solution {
-    
     public int solution(int m, int n, int[][] puddles) {
-        int[][] featMap = new int[n][m];
-        
-        // 웅덩이
-        for (int i = 0; i < puddles.length; i++) {
-            int y = puddles[i][1] - 1; 
-            int x = puddles[i][0] - 1; 
-            featMap[y][x] = -1;        
+        // m이 x고 n이 y임
+        int[][] dp = new int[n+1][m+1];
+        boolean[][] isPuddle = new boolean[n+1][m+1];
+        for (int[] puddle : puddles) {
+            int x = puddle[0];
+            int y = puddle[1];
+            isPuddle[y][x] = true;
         }
         
-        
-        for (int i = 0; i < n; i++) {
-            if (featMap[i][0] == -1) {
-                break; 
-            }
-            featMap[i][0] = 1;
-        }
-        
-        for (int i = 0; i < m; i++) {
-            if (featMap[0][i] == -1) {
-                break; 
-            }
-            featMap[0][i] = 1;
-        }
-
-        
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (featMap[i][j] == -1) {
-                    continue; 
+        dp[1][1]=1;
+        for(int y=1;y<=n;y++){
+            for(int x=1;x<=m;x++){
+                if (isPuddle[y][x]) {
+                    dp[y][x] = 0;
+                    continue;
                 }
-                if (featMap[i-1][j] != -1) {
-                    featMap[i][j] += featMap[i-1][j] % 1000000007;
-                }
-                if (featMap[i][j-1] != -1) {
-                    featMap[i][j] += featMap[i][j-1] % 1000000007;
-                }
-                featMap[i][j] %= 1000000007;
+                if(x>1){dp[y][x]=(dp[y][x]+dp[y][x-1])%1000000007;}
+                if(y>1){dp[y][x]=(dp[y][x]+dp[y-1][x])%1000000007;}                
             }
         }
-
-        return featMap[n-1][m-1]; 
+        return dp[n][m];
+        
+        
+        
+        
     }
 }
