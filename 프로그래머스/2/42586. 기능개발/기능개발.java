05afb3293dfcb27cfba74dcbcf75e302e41e6da0
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-04-21 10:58:55 +0900

    Update 기능개발.java
    다시 풀어봄

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,36 +1,37 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int  max = 100;
-        int[] finish = new int[progresses.length];
+                
+        List<Integer> result = new ArrayList<>();
         
-        for(int i=0;i<finish.length;i++){
-            int left = max-progresses[i];
-            finish[i] = (left + speeds[i] - 1) / speeds[i];
+        Queue<Integer> days = new LinkedList<>();
+        for(int i=0;i<progresses.length;i++){
+            int remain = 100 - progresses[i];
+            int day = (int) Math.ceil((double)remain/speeds[i]);
+            days.offer(day);
         }
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        int line = finish[0];
-        int cnt=1;
+        int baseDay = days.poll();
+        int count =1;
         
-        for(int i=1;i<finish.length;i++){
-            if(line<finish[i]){
-                arr.add(cnt);
-                cnt=1;
-                line = finish[i];
+        while(!days.isEmpty()){
+            int current = days.poll();
+            if(current<=baseDay){
+                count++;
             }
             else{
-                cnt++;
+                result.add(count);
+                baseDay=current;
+                count=1;
             }
         }
-        arr.add(cnt);
+        result.add(count);
         
-        int[] ret = new int[arr.size()];
-        int idx=0;
-        for(Integer val : arr){
-            ret[idx++]=val;
+        int[] ret = new int[result.size()];
+        for(int i=0;i<ret.length;i++){
+            ret[i]=result.get(i);
         }
-        return ret;
+        return ret;       
+        
     }
 }
