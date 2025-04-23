7abd0cdfe3ba5bfac174fc9cbd9a5bcd6afd5f11
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-04-23 11:20:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.30 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,42 @@
 import java.util.*;
 
 class Solution {
+    static final int[] dy = {-1,0,1,0};
+    static final int[] dx = {0,1,0,-1};
     
-    static final int[] dy = {-1, 0, 1, 0}; 
-    static final int[] dx = {0, 1, 0, -1}; 
-    
+    private class Node{
+        int x;
+        int y;    
+        
+        public Node(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
     public int solution(int[][] maps) {
-        
-        Queue<Integer> yQueue = new LinkedList<>();
-        Queue<Integer> xQueue = new LinkedList<>();
-        
-        int[][] retMap = new int[maps.length][maps[0].length];
-        retMap[0][0]=1;
-        
-        yQueue.offer(0);
-        xQueue.offer(0);
-        while(!yQueue.isEmpty()&&!xQueue.isEmpty()){
-            int y = yQueue.poll();
-            int x = xQueue.poll();
-            
-            for(int i=0;i<4;i++){
+        int n = maps.length;
+        int m = maps[0].length;
+        int[][] vis = new int[n][m];
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.offer(new Node(0, 0));
+        vis[0][0] = 1; // 시작점의 거리를 1로 초기화
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int y = node.y, x = node.x;
+            for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                
-                if(ny<0||ny>=maps.length||nx<0||nx>=maps[0].length||maps[ny][nx]==0||retMap[ny][nx]!=0){
-                    continue;
-                }
-                if (retMap[ny][nx] == 0) { // 처음 방문하는 경우만 갱신
-                    retMap[ny][nx] = retMap[y][x] + 1;
-                    yQueue.offer(ny);
-                    xQueue.offer(nx);
-                }                
-            }           
-            
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;     // 범위체크
+                if (maps[ny][nx] == 0) continue;                          // 벽 체크
+                if (vis[ny][nx] != 0) continue;                           // 이미 방문
+                vis[ny][nx] = vis[y][x] + 1;                              // 거리 갱신
+                queue.offer(new Node(nx, ny));                            // 큐에 추가
+            }
         }
-        return retMap[maps.length-1][maps[0].length-1]==0?-1:retMap[maps.length-1][maps[0].length-1];
-       
+
+        int answer = vis[n-1][m-1];
+        return answer == 0 ? -1 : answer;
     }
 }
