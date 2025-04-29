c4cec4f2f25260fd12da819433c6c8a5710be769
Changmin Kim <116146234+Rlackdals981010@users.noreply.github.com>
2025-04-29 11:12:52 +0900

    [level 2] Title: 타겟 넘버, Time: 8.36 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,12 +1,20 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        return findTarget(0,0,numbers,target);        
-    }
-    private int findTarget(int idx,int now,int[] numbers,int target){
-        if(idx ==numbers.length){
-            if(now==target) return 1;
-            return 0;
+        int remain=0;
+        for(int num:numbers){
+            remain+=num;
         }
-        return findTarget(idx+1,now-numbers[idx],numbers,target)+findTarget(idx+1,now+numbers[idx],numbers,target);
+        return dfs(numbers,target,0,0,remain);
+        
     }
+    public int dfs(int[]numbers,int target, int depth, int sum, int remain){
+        if(depth==numbers.length){
+            if(sum==target){
+                return 1;
+            }
+            else return 0;
+        }
+        return dfs(numbers,target,depth+1,sum+numbers[depth],remain-numbers[depth])+dfs(numbers,target,depth+1,sum-numbers[depth],remain-numbers[depth]);
+    }
+    
 }
